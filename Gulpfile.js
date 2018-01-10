975d20ba5db11be775369d161cf050e87dcb592c
Elkin Hurtado <syselkin5@gmail.com>
2018-01-09 19:25:57 -0500

    Crear vistas, ordenar la carpeta public, crear favicon
    Crear y desplegar vistas con pug y express, ordenar que lo que se pone en public se genere por medio de gulp. crear y poner los favicon a nuetra aplicacion de manera que soporte todos los escritorios y dispositivos.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,12 +5,18 @@ var rename = require('gulp-rename');
 // recibe el nombre de la tarea y la funcion
 // vamos a pedirle que coja un archivo y lo pase por el preprocesador de sass
 gulp.task('styles', function () {
-    gulp
-        .src('index.scss')
-        .pipe(sass())
-        .pipe(rename('app.css'))
-        .pipe(gulp.dest('resources/css'))
+  gulp
+    .src('index.scss')
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('assets', function () {
+  gulp
+    .src('assets/*')
+    .pipe(gulp.dest('public'))
 })
 
 //Tarea por defecto, recible un array que puede ejecutar varias tareas
-gulp.task('default', ['styles'])
+gulp.task('default', ['styles', 'assets'])
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 // SIRVE PARA CREAR SERVER Y LEVANTAR
 
 var express = require('express');
-
 var app = express();
+
+//Se le dice a la libreria express que procese las vistas con pug
+app.set('view engine', 'pug');
+
+//Se declara las carpetas donde busca los recursos
+app.use(express.static('public'));
 
 //cuando se acceda a dicha ruta se ejecute esa funcion
 app.get('/', function (req, res) {
-    res.send('Hola Mundo');
+  res.render('index');
 })
 
 //Se pone el puerto donde se ejecuta y es posible tambien la url, y la funcion en la cual se va a ejecutar si no se ejecuta bien
 app.listen(3000, function (err) {
-    if (err) return console.log('Hubo un error'), process.exit(1);
+  if (err) return console.log('Hubo un error'), process.exit(1);
 
-    console.log('Platzigram escuchando en el puerto 3000')
-})
+  console.log('Platzigram escuchando en el puerto 3000')
+})  
