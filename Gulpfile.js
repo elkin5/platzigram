da99cb48c7136685b0f78a743d7543637e2ee514
Elkin Hurtado <syselkin5@gmail.com>
2018-01-09 21:15:37 -0500

    babel
    npm install --save-dev babel-core

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 // recibe el nombre de la tarea y la funcion
 // vamos a pedirle que coja un archivo y lo pase por el preprocesador de sass
@@ -18,5 +21,16 @@ gulp.task('assets', function () {
     .pipe(gulp.dest('public'))
 })
 
+//le pedimos que lo procese con babel
+// y que generfe un bundle (resumen)
+gulp.task('scripts', function () {
+  browserify('./src/index.js')
+    .transform(babel)  
+    .bundle()         
+    .pipe(source('index.js'))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('public'))
+})
+
 //Tarea por defecto, recible un array que puede ejecutar varias tareas
-gulp.task('default', ['styles', 'assets'])
+gulp.task('default', ['styles', 'assets', 'scripts'])
