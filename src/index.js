7487132b1cb3e7b6b9a39adc93fe422d60be825c
Elkin Hurtado <syselkin5@gmail.com>
2018-01-10 20:29:16 -0500

    Agregando navegacion
    Se usa la libreria page  para crear link que no rendeerizan a una pagina en blanco nsi no que se usa en la misma pagina

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert('hola')
+var page = require('page');
+
+var main = document.getElementById('main-container'); 
+
+page('/', function (ctx, next) {
+  main.innerHTML = 'Home <a href="/Signup">Signup</a> Home <a href="/Signin">Signin</a>';
+})
+
+page('/signup', function (ctx, next) {
+  main.innerHTML = 'Signup <a href="/">Home</a>';
+})
+
+page('/signin', function (ctx, next) {
+  main.innerHTML = 'Signin <a href="/No-exixte">No existe</a>';
+})
+
+page.start();
