2e98b7a1057fad664e03379fd8cc8d319001d68d
Elkin Hurtado <syselkin5@gmail.com>
2018-01-22 20:48:20 -0500

    Agregando paginas a la navegacion.
    Se utiliza la libreria yo-yo para agregar paginas al DOM de manera facil.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 var page = require('page');
+var yo = require('yo-yo');
+var empty = require('empty-element');
 
 var main = document.getElementById('main-container'); 
 
@@ -7,7 +9,47 @@ page('/', function (ctx, next) {
 })
 
 page('/signup', function (ctx, next) {
-  main.innerHTML = 'Signup <a href="/">Home</a>';
+  var el = yo`<div class="container">
+      <div class="row">        
+        <div class="col s10 push-s1">
+          <div class="row">
+            <div class="col m5 hide-on-small-only">
+              <img class="celular" src="celular.png" alt="celular">
+            </div>
+            <div class="col s12 m7">
+              <div class="row">
+                <div class="signup-box">
+                  <h1 class="platzigram">Platzigram</h1>
+                  <form class="signup-form">
+                    <h2>Registrarse</h2>
+                    <div class="section">                      
+                      <a class="btn btn-fb hide-on-small-only">Iniciar sesion con facebook</a>
+                      <a class="btn btn-fb hide-on-med-and-up">Iniciar sesion</a>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                      <input type="email" name="email" placeholder="Correo electronico">
+                      <input type="text" name="name" placeholder="Nombre completo">
+                      <input type="text" name="username" placeholder="Usuario">
+                      <input type="password" name="password" placeholder="Contrasena">
+                      <button class="btn waves-effect waves-ligth btn-signup" type="submit">Registrarse</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="row">
+                <div class="login-box">
+                  Tienes una cuenta?
+                  <a href="/signin">Entrar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+    empty(main).appendChild(el);
 })
 
 page('/signin', function (ctx, next) {
